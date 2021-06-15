f481f839541632303c969467a73e10fe9aa3f134
Arttu Mäkinen <ignomagames@gmail.com>
2021-06-15 15:04:57 +0300

    HyperMD integrated to ReactCodeMirror

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,14 +1,56 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import React from 'react'
 import CodeMirror from 'react-codemirror'
-require('codemirror/lib/codemirror.css');
+import 'hypermd'
+import 'hypermd/core';
+import 'hypermd/mode/hypermd';
+import 'codemirror/lib/codemirror.css'
 
 
 const Editor = () => {
-  const [text, setText] = useState('moi :3')
+  const [text, setText] = useState(`
+# Otsikko
+*moi*
+**hei**
+  `)
+
+  const editorRef = useRef()
+
+  const options = {
+    lineNumbers: false,
+    smartIndent: true,
+    tabSize: 4,
+    indentWithTabs: false,
+    lineWrapping: true,
+    mode: 'hypermd',
+    theme: 'hypermd-light',
+    hmdFold: {
+      image: true,
+      link: true,
+      math: true,
+    },
+    hmdHideToken: true,
+    hmdCursorDebounce: true,
+    hmdPaste: true,
+    hmdClick: true,
+    hmdHover: true,
+    hmdTableAlign: true,
+    // autoFocus: true
+    // gutters: []
+    // scrollbarStyle
+    // readOnly: true
+  }
 
   return (
-    <CodeMirror value={text} onChange={setText} options={{ lineNumbers: true }} />
+    <CodeMirror
+      value={text}
+      onChange={setText}
+      options={options}
+      ref={editorRef}
+      autoFocus={true}
+      className='moih'
+      codeMirrorInstance={ require('codemirror') }
+    />
   )
 }
 
